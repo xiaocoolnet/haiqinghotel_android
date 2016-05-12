6ad35b7176ec394608160bf2888bab6635748472
王志恒 <960753473@qq.com>
2016-05-12 13:10:50 +0800

    加入日历选择器

--- app/src/main/java/cn/xiaocool/haiqinghotel/main/homepage/RoomIntroActivity.java
+++ app/src/main/java/cn/xiaocool/haiqinghotel/main/homepage/RoomIntroActivity.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -29,10 +30,12 @@ import cn.xiaocool.haiqinghotel.utils.IntentUtils;
 public class RoomIntroActivity extends Activity implements View.OnClickListener {
     private TextView tvTitle;
     private RelativeLayout btnExit;
+    private LinearLayout llDateChoose;
     private String roomId,roomName;
     private Context context;
+    private final int CODE = 1;
     private TextView tvOPrice, tvPrice, tvNetwork, tvWindow, tvPeopleNum, tvBathroom,
-            tvRoomFloor, tvRoomArea, tvBedSize,tvTotalPrice;
+            tvRoomFloor, tvRoomArea, tvBedSize,tvTotalPrice,tvInday,tvOutday,tvDayCount;
     private Button btnReserve;
     private Handler handler = new Handler() {
         public void handleMessage(Message msg) {
@@ -93,6 +96,22 @@ public class RoomIntroActivity extends Activity implements View.OnClickListener 
         initView();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CODE){
+            String indayMonNum = data.getStringExtra("inDayMonth1");
+            String indayDayNum = data.getStringExtra("inDayNum0");
+            String outdayMonNum = data.getStringExtra("outMonth1");
+            String outdayDayNum = data.getStringExtra("outDayNum0");
+            String dayCount = data.getStringExtra("dayCount");
+            Log.e("五个数：",indayMonNum + indayDayNum + outdayMonNum + outdayDayNum + dayCount);
+            tvInday.setText("入住：" + indayMonNum + "月" + indayDayNum);
+            tvOutday.setText("离店：" + outdayMonNum + "月" + outdayDayNum);
+            tvDayCount.setText("共" + dayCount + "晚");
+        }
+    }
+
     private void initView() {
         tvTitle = (TextView) findViewById(R.id.top_title);
         tvTitle.setText(roomName);
@@ -110,6 +129,12 @@ public class RoomIntroActivity extends Activity implements View.OnClickListener 
         tvTotalPrice = (TextView) findViewById(R.id.tv_total_price);
         btnReserve = (Button) findViewById(R.id.btn_reserve_now);
         btnReserve.setOnClickListener(this);
+        llDateChoose = (LinearLayout) findViewById(R.id.reserve_choose_date);
+        llDateChoose.setOnClickListener(this);
+        //日期
+        tvInday = (TextView) findViewById(R.id.tv_inday);
+        tvOutday = (TextView) findViewById(R.id.tv_outday);
+        tvDayCount = (TextView) findViewById(R.id.tv_day_count);
         new HomepageRequest(this, handler).roomDetails(roomId);
     }
 
@@ -121,6 +146,10 @@ public class RoomIntroActivity extends Activity implements View.OnClickListener 
                 break;
             case R.id.btn_reserve_now:
                 IntentUtils.getIntent(this,BookingNowActivity.class);
+                break;
+            case R.id.reserve_choose_date:
+                Intent intent = new Intent(this,CalenderMainActivity.class);
+                startActivityForResult(intent, CODE);
                 break;
         }
     }
